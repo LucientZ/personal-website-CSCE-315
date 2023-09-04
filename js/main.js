4e748cfd7356d5c5e22eb2799e39a101d1ed23a0
LucientZ <koblenzpoisiden@gmail.com>
2023-09-04 16:27:23 -0500

    Added basic css, javascript, and html

---
+++ js/main.js
@@ -0,0 +1,33 @@
+/**
+ * Modifies main-css theme to link to css stored in localStorage item "CSShref"
+ * If 
+ */
+updateTheme = () => {
+    const theme = document.getElementById("theme")
+    const href = localStorage.getItem("CSShref");
+    if (href) {
+        theme.setAttribute('href', href);
+    }
+    else {
+        localStorage.setItem("CSShref", "./css/sunshine.css");
+        theme.setAttribute('href', "./css/sunshine.css");
+    }
+};
+
+/**
+ * Toggles theme stored in localStorage item "CSShref" and updates document to have said theme
+ */
+toggleTheme = () => {
+    const href = localStorage.getItem("CSShref");
+    switch (href) {
+        case "./css/sunshine.css":
+            localStorage.setItem("CSShref", "./css/moonlight.css");
+            break;
+        case "./css/moonlight.css":
+            localStorage.setItem("CSShref", "./css/sunshine.css");
+            break;
+        default:
+            localStorage.setItem("CSShref", "./css/sunshine.css")
+    }
+    updateTheme();
+}
